38632612ae8ba11b421011f7f18d4d056461f828
Jin Siang Teh <jteh1@my.bcit.ca>
2026-01-18 19:33:17 -0800

    Add files via upload

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -1,1 +1,13 @@
-
+export const MESSAGES = {
+    APP_TITLE: "Lab 1: JSON, Object Constructor, Local Storage",
+    ADD_NOTE: "add",
+    REMOVE_NOTE: "remove",
+    READER_PAGE: "Reader page",
+    WRITER_PAGE: "Writer page",
+    READER: "reader.html",
+    WRITER: "writer.html",
+    STORED: "stored at",
+    UPDATED: "updated at",
+    BACK :" back",
+    INDEX: "index.html"
+};
